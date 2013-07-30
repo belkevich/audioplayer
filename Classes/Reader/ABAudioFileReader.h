5043999bfbac2844db90793d09c4dbfc4da89f53
Alexey Belkevich <belkevich.alexey@gmail.com>
2013-07-30 16:26:46 +0300

    Added removed file

---
+++ Classes/Reader/ABAudioFileReader.h
@@ -0,0 +1,27 @@
+//
+//  ABAudioFileReader.h
+//  ABAudioPlayer
+//
+//  Created by Alexey Belkevich on 7/17/13.
+//  Copyright (c) 2013 okolodev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ABAudioReaderProtocol.h"
+
+
+@interface ABAudioFileReader : NSObject <ABAudioReaderProtocol>
+{
+    AudioFileID audioFile;
+    AudioStreamBasicDescription dataFormat;
+    AudioStreamPacketDescription *packetDescription;
+    char *magicCookie;
+    UInt32 bufferSize;
+    UInt32 packetsToRead;
+    SInt64 packetCount;
+}
+
+- (BOOL)openAudio:(NSString *)path;
+- (void)closeAudio;
+
+@end
